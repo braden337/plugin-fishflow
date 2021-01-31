6c3d953a88a85eab0aca979a7b9369153cff97f8
Braden Jonsson <braden337@gmail.com>
2021-01-30 16:04:14 -0800

    Clean up formatting

--- functions/cf.fish
+++ functions/cf.fish
@@ -1,4 +1,5 @@
 # edit the fish config
+
 function cf
     set conf ~/.config/fish/config.fish
     vim $conf
--- functions/wttr.fish
+++ functions/wttr.fish
@@ -1,4 +1,5 @@
 # get the weather
+
 function wttr
     curl -s 'http://wttr.in' | raku -e '.say for lines[2..6]'
 end
