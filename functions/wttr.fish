950e655c9189a63d46f2bbaae2bbf8c1f56bd936
Braden Jonsson <braden337@gmail.com>
2021-01-31 15:51:32 -0600

    Fix weather raku check

--- functions/wttr.fish
+++ functions/wttr.fish
@@ -3,7 +3,7 @@
 function wttr
     curl -s "http://wttr.in" >/tmp/wttr
 
-    if type -q sdfsdf
+    if type -q raku
         cat /tmp/wttr | raku -e '.say for lines[0..6]'
     else
         cat /tmp/wttr | sed -n '1,7p'
