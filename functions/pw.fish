fdcb76822ebe058d003083980a89136459cbabed
Braden Jonsson <braden337@gmail.com>
2021-01-31 17:33:53 -0600

    Make functions local

--- functions/pw.fish
+++ functions/pw.fish
@@ -1,6 +1,14 @@
 # generate a password
 
 function pw
+    function hex -a n
+        printf %x $n
+    end
+
+    function symbol
+        string unescape "\\x"(hex (random 33 47))
+    end
+
     set -l x (random 1 100)
     set -l y (random 1 100)
     set -l z (random 1 100)
@@ -11,13 +19,6 @@ function pw
     cat /tmp/pw | sed -n -e "$x"p -e "$y"p -e "$z"p | string join $s | string replace $s (symbol)
 
     rm /tmp/pw
-end
 
-
-function hex -a n
-    printf %x $n
-end
-
-function symbol
-    string unescape "\\x"(hex (random 33 47))
+    functions -e hex symbol
 end
