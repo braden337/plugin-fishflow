a56190465b5cc6d01757b610ef1f072e0731b3f2
Braden Jonsson <braden337@gmail.com>
2021-02-11 19:23:24 -0600

    Check if location exists before opening

--- functions/open.fish
+++ functions/open.fish
@@ -1,12 +1,17 @@
 # open files
 
 function open -a place
-    if type -qf open
-        open $place
-    else if type -qf explorer.exe
-        explorer.exe (string trim -c \/ $place) 
+    if test -e $place
+        if type -qf open
+            open $place
+        else if type -qf explorer.exe
+            explorer.exe (string trim -c \/ $place)
+        else
+            echo "Unable to open '$place'"
+            return 1
+        end
     else
-        echo "Unable to open $place"
+        echo "'$place' doesn't exist"
         return 1
     end
 
