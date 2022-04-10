72a119048579d486746e076f047cb0d3eac387b5
Braden Jonsson <braden337@gmail.com>
2022-04-10 01:31:34 -0500

    simplify pw function

--- functions/pw.fish
+++ functions/pw.fish
@@ -1,24 +1,7 @@
 # generate a password
 
-function pw
-    function hex -a n
-        printf %x $n
-    end
-
-    function symbol
-        string unescape "\\x"(hex (random 33 47))
-    end
-
-    set -l x (random 1 100)
-    set -l y (random 1 100)
-    set -l z (random 1 100)
-    set -l s (symbol)
-
-    curl -s "https://www.random.org/passwords/?num=100&len=8&format=plain&rnd=new" >/tmp/pw
-
-    cat /tmp/pw | sed -n -e "$x"p -e "$y"p -e "$z"p | string join $s | string replace $s (symbol)
-
-    rm /tmp/pw
-
-    functions -e hex symbol
+function pw -a n
+    set -q n[1]; or set n 1
+    curl -s 'https://www.random.org/passwords/?num=100&len=24&format=plain&rnd=new' | shuf -n $n
 end
+
