ba7423a8ebf9da39e515e288e0ea6d528bdf131e
Braden Jonsson <braden337@gmail.com>
2022-09-10 15:46:08 -0500

    replace shuf utility with sort (random order) and head

--- functions/pw.fish
+++ functions/pw.fish
@@ -2,6 +2,6 @@
 
 function pw -a n
     set -q n[1]; or set n 1
-    curl -s 'https://www.random.org/passwords/?num=100&len=24&format=plain&rnd=new' | shuf -n $n
+    curl -s 'https://www.random.org/passwords/?num=100&len=24&format=plain&rnd=new' | sort -R | head -n 1
 end
 
